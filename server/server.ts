bf63969d140b64bb628556b98e30313f0df06647
vtipo_sh_nik <akzhol.kasym2004@mail.ru>
2024-05-01 07:23:50 +0600

    mongoDB connected

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import express, { Router } from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import bodyParser from 'body-parser';
-
+import mongoose from 'mongoose';
 
 // middleware
 const app = express()
@@ -10,6 +10,19 @@ const app = express()
 app.use(bodyParser.json());
 app.use(cors());
 dotenv.config()
+// console.log(process.env.PASSWORD_DB);
+
+if (process.env.MONGO_URL) {
+    mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("MongoDB connected successfully!");
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+} else {
+    console.log('environment variable is undefined!');
+}
 
 // vars
 const port = 3307
