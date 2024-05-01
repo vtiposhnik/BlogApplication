5e2a702f4ad87d5cb0725e6f978712f6890e72a8
vtipo_sh_nik <akzhol.kasym2004@mail.ru>
2024-05-01 07:49:59 +0600

    User model created

---
+++ server/models/user.model.ts
@@ -0,0 +1,27 @@
+import { Timestamp } from "mongodb";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+const User = new mongoose.Model('User',userSchema)
+
+export default User;
